d5fe478560b2c7f3bbe2eb435104d4fbf7ca137f
Manish Sharma <badgotra.manish@gmail.com>
2025-02-10 21:28:04 +0530

    copied from 'Run a program' code examples

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,79 +4,52 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"strconv"
 	"strings"
 )
 
-type (
-	cmdFnc func([]string)
-)
-
-var commands = make(map[string]cmdFnc)
-
-func initCommands() {
-	registerCommand("exit", exit)
-	registerCommand("echo", echo)
-	registerCommand("type", typer)
-}
-
 func main() {
-	initCommands()
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		// Wait for user input
 		in, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			fmt.Printf("error reading from stdin: %s", err.Error())
-			os.Exit(1)
+			fmt.Println(err.Error())
 		}
-		inputs := strings.Split(strings.TrimSpace(in), " ")
-		cmd := inputs[0]
-		args := inputs[1:]
-		cmdFn, ok := commands[cmd]
-		if !ok {
-			notFound(cmd)
-		} else {
-			cmdFn(args)
-		}
-	}
-}
-
-func registerCommand(cmd string, fn cmdFnc) {
-	commands[cmd] = fn
-}
-
-func notFound(cmd string) {
-	fmt.Printf("%s: command not found\n", cmd)
-}
-func exit(args []string) {
-	if len(args) == 0 {
-		os.Exit(1)
-	}
-	if code, err := strconv.Atoi(args[0]); err == nil {
-		os.Exit(code)
-	}
-}
-func echo(args []string) {
-	fmt.Println(strings.Join(args, " "))
-}
-func typer(args []string) {
-	if len(args) == 0 {
-		fmt.Println("")
-	}
-	_, builtin := commands[args[0]]
-	if builtin {
-		fmt.Printf("%s is a shell builtin\n", args[0])
-		return
-	}
-	paths := strings.Split(os.Getenv("PATH"), ":")
-	for _, path := range paths {
-		fp := filepath.Join(path, args[0])
-		if _, err := os.Stat(fp); err == nil {
-			fmt.Println(fp)
-			return
+		clear_in := strings.TrimRight(in, "\n")
+		cmds := strings.Split(clear_in, " ")
+		switch cmds[0] {
+		case "exit":
+			os.Exit(0)
+		case "echo":
+			fmt.Println(strings.Join(cmds[1:], " "))
+		case "type":
+			switch cmds[1] {
+			case "exit", "echo", "type":
+				fmt.Printf("%s is a shell builtin\n", cmds[1])
+			default:
+				paths := strings.Split(os.Getenv("PATH"), ":")
+				isFound := false
+				for _, path := range paths {
+					fullPath := filepath.Join(path, cmds[1])
+					if _, err := os.Stat(fullPath); !os.IsNotExist(err) {
+						fmt.Printf("%s is %s\n", cmds[1], fullPath)
+						isFound = true
+						break
+					}
+				}
+				if !isFound {
+					fmt.Printf("%s: not found\n", cmds[1])
+				}
+			}
+		default:
+			command := exec.Command(cmds[0], cmds[1:]...)
+			command.Stderr = os.Stderr
+			command.Stdout = os.Stdout
+			err := command.Run()
+			if err != nil {
+				fmt.Printf("%s: command not found\n", cmds[0])
+			}
 		}
 	}
-	fmt.Printf("%s not found\n", args[0])
 }
