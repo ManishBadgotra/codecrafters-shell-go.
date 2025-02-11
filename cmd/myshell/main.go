d396e9db667f0ebb6d0b2b2e2609060f06c91009
Manish Sharma <badgotra.manish@gmail.com>
2025-02-11 19:48:43 +0530

    cd builtin: Absolute paths -- correction in error handling v2

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -50,7 +50,7 @@ func main() {
 			fmt.Println(dir)
 		case "cd":
 			if err := os.Chdir(cmds[0]); err != nil {
-				fmt.Fprintf(os.Stdout, "cd: %s: No such file or directory\n", cmds[0])
+				fmt.Fprintf(os.Stdout, "cd: %s: No such file or directory\n", cmds[1])
 			}
 		default:
 			command := exec.Command(cmds[0], cmds[1:]...)
